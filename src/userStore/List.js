d9eb720dc2b90dfb69e9fe2fbff96856303b7cdb
Lin <linoravny@users.noreply.github.com>
2021-02-28 15:47:09 +0200

    fetch data with axios

--- src/userStore/List.js
+++ src/userStore/List.js
@@ -1,27 +1,57 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import data from '../data/book.json';
 import { Image, Box, Card, Text} from 'rebass';
+import axios from 'axios';
 
-// user props data object - one way data flow
 function List() {
+  //from store - REDUX
   const isUserActive = useSelector(state => state.isLoggedIn);
 
+  //hook useState
+  const [books, setData] = useState([]); //add state to function components
+  const [isLoading, setLoading] = useState(false); 
 
-    // axios.get('../data/book.json')
-    //   .then(res => console.log(res.data))
-    //   .catch(err => console.log(err)
+  //fires after layout and paint
+  useEffect(() => {
+    const apiUrl = '/data/book.json';
+    
+    // const fetchData = async () => {
+    //   const result = await axios(
+    //     apiUrl,
+    //   );
+    //   setData(result.data.items);
+    // };
+ 
+    // fetchData();
 
-  
+    setLoading(true);
+    axios.get(apiUrl).then((repos) => {
+      //setTimeout(function(){ //for check loading
+        console.log("repos.data:" + JSON.stringify(repos.data));
+        setData(repos.data.items);
+        setLoading(false);
+      //}, 2000);
+      
+    }).catch((err) => {
+      console.log("FETCH_DATA_REJECTED err" + err);
+      setLoading(false);
+    });
+
+  },[]);
+
   return (
     <div>
       <h3>Guitar List</h3>
       {
       isUserActive ?  
+
       <div>
         <h3>BOOK LIST</h3>
+        { isLoading  && 
+          <Box>loading...</Box>
+        }
         <Box>
-          {data.items.map((book, index) => {
+          {books.map((book, index) => {
             return (
               <Card width={256} key={book.id}>
                 <Image
